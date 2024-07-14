cd29ba056965acd890f7ae8403dc7fabcf76dcd3
seungin.lee <seungin.lee8@gmail.com>
2024-07-15 03:00:41 +0900

    [Update] Add feature for playlist download

--- downloader.py
+++ downloader.py
@@ -11,12 +11,12 @@ from mutagen.id3 import ID3, APIC
 
 
 def clean_url(url):
+    if 'playlist?' in url:
+        return url
+
     # parsing URL
     parsed_url = urlparse(url)
     query_params = parse_qs(parsed_url.query)
-
-    if 'playlist?' in parsed_url.path:
-        return url
 
     # Only keep 'v' parameter in url
     cleaned_params = {}
@@ -63,82 +63,86 @@ def download_audio(url, select_number=0, ffmpeg_path="none", metadata_callback=N
     ydl_opts = {
         'quiet': True,
         'extract_flat': True,
-        'force_generic_extractor': True,
+        'get_id': True,
+        'skip_download': True,
     }
-
+    print(f"select_number is {select_number}")
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=False)
 
-        # Extract metadata
-        title = info.get('title', 'Unknown Title')
-        artist = info.get('uploader', 'Unknown Artist')
-        thumbnail_url = info.get('thumbnail')
-
-        # Download thumbnail_url for sending to gui via callback
-        if thumbnail_url:
-            response = requests.get(thumbnail_url)
-            thumbnail = Image.open(BytesIO(response.content))
-            thumbnail = resize_image(thumbnail)
-
-            # Save the thumbnail to a temporary file
-            thumbnail_path = "thumbnail.jpg"
-            thumbnail.save(thumbnail_path)
-        else:
-            thumbnail = None
-
-        if metadata_callback:
-            metadata_callback(title, artist, thumbnail)
-
-
         if 'entries' in info:
             # If url means 'playlist'
-            video_count = len(info['entries'])
-            print(f"The number of tracks in the playlist is {video_count}")
+            video_ids = [entry['id'] for entry in info['entries']]
+            print(f"The number of tracks in the playlist is {len(video_ids)}")
         else:
             # If url means 'single track'
-            video_count = 1
+            video_ids = [info['id']]
             print("This is a single track")
-    for i in range(1, video_count + 1):
-        if int(select_number) == i or int(select_number) == 0:
-            download_file_path = None
-            def progress_hook(d):
-                if d['status'] == 'downloading':
-                    percent = d['_percent_str'].strip('%')
-                    if progress_callback:
-                        progress_callback(int(float(percent)))
-            def postprocessor_hook(d):
-                nonlocal download_file_path
-                if d['status'] == 'finished':
-                    download_file_path = d['info_dict']['filepath']
 
-            ydl_opts = {
-                'format': 'bestaudio/best',
-                'progress_hooks': [progress_hook],
-                'postprocessor_hooks': [postprocessor_hook],
-                'postprocessors': [{
-                    'key': 'FFmpegExtractAudio',
-                    'preferredcodec': 'mp3',
-                    'preferredquality': '0',
-                }, {
-                    'key': 'EmbedThumbnail',
-                }, {
-                    'key': 'FFmpegMetadata',
-                }],
-                'writethumbnail': True,
-                'outtmpl': 'Downloads/%(title)s.%(ext)s',
-                'playliststart': i,
-                'playlistend': i,
-                'postprocessor_args': [
-                    '-metadata', f"track='{i}'"
-                ],
-                'ffmpeg_location': ffmpeg_path,  # Add path of ffmpeg
-            }
+        for i in range(0, len(video_ids)):
+            if int(select_number) == 0  or (i + 1) == int(select_number):
+                print("START LOOP")
+                # Extract metadata
+                info_dict = ydl.extract_info(video_ids[i], download=False)
+                title = [info_dict['title']][0]
+                uploader = [info_dict['uploader']][0]
+                thumbnail_url = [info_dict['thumbnail']][0]
+                print(f"thumbnail image url : {thumbnail_url}")
 
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([url])
-                set_album_art(download_file_path, thumbnail_path)
+                # Download thumbnail_url for sending to gui via callback
+                if thumbnail_url:
+                    response = requests.get(thumbnail_url)
+                    thumbnail = Image.open(BytesIO(response.content))
+                    thumbnail = resize_image(thumbnail)
 
-    if thumbnail_path and os.path.exists(thumbnail_path):
-        os.remove(thumbnail_path)
+                    # Save the thumbnail to a temporary file
+                    thumbnail_path = "thumbnail.jpg"
+                    thumbnail.save(thumbnail_path)
+                else:
+                    thumbnail = None
+
+                if metadata_callback:
+                    metadata_callback(title, uploader, thumbnail)
+
+                download_file_path = None
+                def progress_hook(d):
+                    if d['status'] == 'downloading':
+                        percent = d['_percent_str'].strip('%')
+                        if progress_callback:
+                            progress_callback(int(float(percent)))
+                def postprocessor_hook(d):
+                    nonlocal download_file_path
+                    if d['status'] == 'finished':
+                        download_file_path = d['info_dict']['filepath']
+
+                ydl_opts = {
+                    'format': 'bestaudio/best',
+                    'progress_hooks': [progress_hook],
+                    'postprocessor_hooks': [postprocessor_hook],
+                    'postprocessors': [{
+                        'key': 'FFmpegExtractAudio',
+                        'preferredcodec': 'mp3',
+                        'preferredquality': '0',
+                    }, {
+                        'key': 'EmbedThumbnail',
+                    }, {
+                        'key': 'FFmpegMetadata',
+                    }],
+                    'writethumbnail': True,
+                    'outtmpl': 'Downloads/%(title)s.%(ext)s',
+                    'playliststart': i+1,
+                    'playlistend': i+1,
+                    'postprocessor_args': [
+                        '-metadata', f"track='{i+1}'"
+                    ],
+                    'ffmpeg_location': ffmpeg_path,  # Add path of ffmpeg
+                }
+
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    ydl.download(video_ids[i])
+                    set_album_art(download_file_path, thumbnail_path)
+
+                if thumbnail_path and os.path.exists(thumbnail_path):
+                    os.remove(thumbnail_path)
 
     return "Download completed successfully!"
